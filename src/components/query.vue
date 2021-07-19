9d5041c6d1a1278c2fc5dabac6d8f5ac543645bf
zky <597768024@qq.com>
2021-07-19 15:01:31 +0800

    add query

---
+++ src/components/query.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <el-form
+      label-width="150px"
+      :inline="true"
+      style="width: 70%; margin: auto"
+    >
+      <el-form-item label="Building Name">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="query">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="tableData" border style="width: 70%; margin: auto">
+      <el-table-column prop="index" label="index" width="100"></el-table-column>
+      <el-table-column prop="type" label="Type" width="180"> </el-table-column>
+      <el-table-column prop="floor" label="Floor" width="180">
+      </el-table-column>
+      <el-table-column prop="path" label="Path"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      options: [],
+      value: '',
+      tableData: [],
+      infos: null
+    }
+  },
+  methods: {
+    getInfo () {
+      this.$axios.get('/infos').then(response => {
+        console.log('res', response)
+        this.infos = response.data
+        const bids = response.data
+        var option = {}
+        for (var i = 0; i < bids.length; i++) {
+          option = {
+            value: i.toString(),
+            label: bids[i].bid
+          }
+          this.options.push(option)
+        }
+      })
+    },
+    query () {
+      var features = this.infos[parseInt(this.value)].features
+      var obj
+      for (var i = 0; i < features.length; i++) {
+        obj = {
+          index: i + 1,
+          type: features[i].type,
+          floor: features[i].fid,
+          path: features[i].path
+        }
+        this.tableData.push(obj)
+      }
+    }
+  },
+  created () {
+    this.getInfo()
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Index from '../components/Index.vue'
+import upLoad from '../components/upLoad.vue'
+import index from '../components/index.vue'
+import query from '../components/query.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Index',
-    component: Index
+    redirect: '/upload',
+    name: 'index',
+    component: index,
+    children: [{
+      path: '/upload',
+      name: 'upLoad',
+      component: upLoad
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: query
+    }]
+
   }
 ]
 
